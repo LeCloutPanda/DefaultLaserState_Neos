9ca64cbdfec577ea8ff4def0c0f7a1d93c0ac7e3
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2022-06-20 12:53:03 +1000

    Initial Commit

---
+++ Patch.cs
@@ -0,0 +1,43 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+
+namespace DefaultLaserState
+{
+    public class Patch : NeosMod
+    {
+        public override string Name => "DefaultLaserState";
+        public override string Author => "LeCloutPanda";
+        public override string Version => "1.0.0";
+
+        public static ModConfiguration config;
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<bool> LASER_STATE = new ModConfigurationKey<bool>("Default Laser State", "", () => true);
+
+        public override void OnEngineInit()
+        {
+            config = GetConfiguration();
+            config.Save(true);
+
+            Harmony harmony = new Harmony($"net.{Author}.{Name}");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(CommonTool), "OnAwake")]
+        class CommonToolPatch
+        {
+            [HarmonyPrefix]
+            static void Prefix(CommonTool __instance, Sync<bool> ____laserEnabled)
+            {
+                if (__instance.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser)
+                    return;
+
+                __instance.RunInUpdates(3, () =>
+                {
+                    ____laserEnabled.Value = config.GetValue(LASER_STATE);
+                });
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DefaultLaserState
- Change the default state of the laser
+ [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos](https://neos.com/) that adds a default state to the Interaction Laser with a config option
+ 
+# Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [DefaultLaserState.dll]() into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+
+# Requirements
+[NeosModSettings](https://github.com/badhaloninja/NeosModSettings)
